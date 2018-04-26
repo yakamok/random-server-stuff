5865857a82762e055be2a1143424f463abd26f8f
yakamok <38737288+yakamok@users.noreply.github.com>
2018-04-27 01:21:05 +0200

    Update dns-monitor.py

--- dns-monitor.py
+++ dns-monitor.py
@@ -1,15 +1,17 @@
 from scapy.all import *
 import socket
-import datetime
+import time
 
-file_dump = "sniffy.dump"
+interface_name = "eth0"
+
+file_dump = "dns-dump"
 if os.path.exists(file_dump) != True:
 	open(file_dump,'w').close()
 
-def find_dns_requests(pkt):
+def find_dns_requests(pkt): #checking to see if any dns requests found then record them to file
 	if pkt.haslayer(DNS) and pkt.getlayer(DNS).qr == 0:
-		with open(file_dump,"a") as handle:
-			handle.write(str(datetime.datetime.now()) + " --- " + str(pkt.getlayer(DNS).qd.qname) + "\n")
-		print str(pkt.getlayer(DNS).qd.qname)
+		with open("dns-dump","a") as handle:
+			handle.write(str(pkt.getlayer(DNS).qd.qname) + " -- " + str(pkt.addr2)  + " -- " + str(pkt.getlayer(IP).src) + "\n")
+		print str(pkt.getlayer(DNS).qd.qname) + " -- " + str(pkt.addr2)  + " -- " + str(pkt.getlayer(IP).src) + " -- " + str(datetime.datetime.now()) + "\n"
 
-sniff(iface="eth0", prn = find_dns_requests, filter = 'dst port 53 ',store=0)
+sniff(iface=interface_name, prn = find_dns_requests, filter = 'dst port 53 ',store=0)
