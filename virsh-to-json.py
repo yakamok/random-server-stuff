625a3f751d4f046b29137c218dc7c4540e764828
yakamok <38737288+yakamok@users.noreply.github.com>
2018-05-15 23:39:51 +0200

    Update virsh-to-json.py

--- virsh-to-json.py
+++ virsh-to-json.py
@@ -5,14 +5,29 @@ import sys
 import os
 import paramiko
 
-username = 'root' #make sure the correct keys are available
+username = 'root' #this depends on the correct keys being available
 cmd = 'virsh list --all'
 for_json_dict = {}
 failed_connections = []
-hosts_file = "/path/to/host-list"
 
-with open(host_file,"r") as handle:
+with open("hostnames","r") as handle:
 	host_ip_list = [x.strip() for x in handle.readlines()]
+
+def parse_output(stdout_data):
+	data = [f.strip() for f in stdout_data]
+	data.pop(len(data) -1)
+	#sort states and calculate the number of stats and assign to host, also preparing to dump into a json file
+	state_dict = {}
+	for d in data:
+		state_dict[d.split()[1]] = d.split()[2]
+
+	temp_dict = {}
+	for key,value in state_dict.iteritems():
+		if value not in temp_dict:
+			temp_dict[value] = 1
+		else:
+			temp_dict[value] += 1
+		for_json_dict[x] = temp_dict
 
 for x in host_ip_list:
 	#establish ssh connection
@@ -20,26 +35,14 @@ for x in host_ip_list:
 		ssh = paramiko.SSHClient()
 		ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 		ssh.connect(x,username=username)
+		
 		ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd)#command being sent
-
-		data = [f.strip() for f in ssh_stdout.readlines()[2:]]
-		data.pop(len(data) -1)
-
-		state_dict = {}
-		for d in data:
-			state_dict[d.split()[1]] = d.split()[2]
-
-		temp_dict = {}
-
-		#sort states and calculate the number of stats and assign to host, also preparing to dump into a json file
-		for key,value in state_dict.iteritems():
-			if value not in temp_dict:
-				temp_dict[value] = 1
-			else:
-				temp_dict[value] += 1
-			for_json_dict[x] = temp_dict
+	 	parse_output(ssh_stdout.readlines()[2:])
+	
 	except:
 		failed_connections.append(x)
+
+print for_json_dict 
 
 #write failed to parse files to file
 if len(failed_connections) > 0:
