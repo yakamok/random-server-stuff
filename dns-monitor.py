22a23f26b04fcef6a57302d648b9b7e38d2e702f
yakamok <38737288+yakamok@users.noreply.github.com>
2018-06-11 01:26:08 +0200

    Update dns-monitor.py

--- dns-monitor.py
+++ dns-monitor.py
@@ -5,10 +5,11 @@ import time
 interface_name = "eth0"
 
 file_dump = "dns-dump"
-if os.path.exists(file_dump) != True:
+if not os.path.exists(file_dump):
 	open(file_dump,'w').close()
 
-def find_dns_requests(pkt): #checking to see if any dns requests found then record them to file
+#checking to see if any dns requests found then record them to file
+def find_dns_requests(pkt): 
 	if pkt.haslayer(DNS) and pkt.getlayer(DNS).qr == 0:
 		with open("dns-dump","a") as handle:
 			handle.write(str(pkt.getlayer(DNS).qd.qname) + " -- " + str(pkt.addr2)  + " -- " + str(pkt.getlayer(IP).src) + "\n")
