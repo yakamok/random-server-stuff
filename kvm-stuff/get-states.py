cef2f39e7637c9c4d4d45f198da217860a7df8e5
yakamok <38737288+yakamok@users.noreply.github.com>
2018-06-11 00:54:16 +0200

    Update get-states.py

--- kvm-stuff/get-states.py
+++ kvm-stuff/get-states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 import json
-import sys
 import libvirt
 
 username = 'bob'
@@ -8,35 +7,40 @@ for_json_dict = {}
 failed_connections = []
 
 #converting numerical value of state to human readable
-states = {0:"nostate",1:"running",2:"blocked",3:"paused",4:"shutdown",5:"shut off",6:"crashed",7:"suspended",8:"last?"}
+states = {0:"nostate", 1:"running", 2:"blocked", 3:"paused", 4:"shutdown", 5:"shut off",\
+            6:"crashed", 7:"suspended", 8:"last?"}
 
-with open("hostnames","r") as handle:
-	host_ip_list = [x.strip() for x in handle.readlines()]
+with open("hostnames", "r") as handle:
+    host_ip_list = [x.strip() for x in handle.readlines()]
 
 for x in host_ip_list:
-	try:
-		temp_dict = {}
-		#connection readonly
-		conn = libvirt.openReadOnly('qemu+ssh://' + username + '@' + x + '/system')
-		#get the list of stats for this host
-		for d in conn.listDefinedDomains():
-			state, _ = conn.lookupByName(d).state()
-			if states[state] not in temp_dict:
-				temp_dict[states[state]] = 1
-			else:
-				temp_dict[states[state]] += 1
-		for_json_dict[x] = temp_dict
-		conn.close()
+    try:
+        temp_dict = {}
 
-	except:
-		failed_connections.append(x)
+        #connection readonly
+        conn = libvirt.openReadOnly('qemu+ssh://' + username + '@' + x + '/system')
+
+        #get the list of stats for this host
+        for d in conn.listDefinedDomains():
+            state, _ = conn.lookupByName(d).state()
+
+            if states[state] not in temp_dict:
+                temp_dict[states[state]] = 1
+            else:
+                temp_dict[states[state]] += 1
+        for_json_dict[x] = temp_dict
+
+        conn.close()
+
+    except:
+        failed_connections.append(x)
 
 #log failed connections
-if len(failed_connections) > 0:
-	print "failed to connect to host(s), please check fail-conns.log"
-	with open("failed-conns.log","w") as handle:
-		[handle.write(x + "\n") for x in failed_connections]
+if failed_connections:
+    print "failed to connect to host(s), please check fail-conns.log"
+    with open("failed-conns.log", "w") as handle:
+        [handle.write(x + "\n") for x in failed_connections]
 
 # #now dump everything into a json file
-with open("host-stats.json","w") as handle:
-	json.dump(for_json_dict,handle)
+with open("host-stats.json", "w") as handle:
+    json.dump(for_json_dict, handle)
